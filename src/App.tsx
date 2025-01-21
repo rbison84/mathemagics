308cf0bf68d43911a0785314dc5cb7b6411839a2
rbison84 <masood.ross@gmail.com>
2025-01-20 20:29:42 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import NumberGuessingGame from './components/NumberGuessingGame';
 
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <NumberGuessingGame />
+    </div>
+  );
+};
+
+export default App;
