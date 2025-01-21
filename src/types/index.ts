56ab309b9677499ef6556d1bce564d8383599028
rbison84 <masood.ross@gmail.com>
2025-01-20 20:23:43 -0500

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,33 @@
+export type CardType = 'power' | 'most-powerful' | 'medium' | 'lower' | 'new';
+export type CardCategory = 'pair-sequence' | 'digit-properties' | null;
 
+export interface ICard {
+  name: string;
+  type: CardType;
+  category: CardCategory;
+  evaluate(number: number, parameter?: number | null): boolean | null;
+}
+
+export interface IDeck {
+  cards: ICard[];
+  draw(): ICard;
+  shuffle(): void;
+}
+
+export interface IGame {
+  deck: IDeck;
+  discardPile: ICard[];
+  computerHand: ICard[];
+  playerHand: ICard[];
+  computerNumber: number;
+  playerNumber: number | null;
+  computerPossibilities: Set<number>;
+  playerPossibilities: Set<number>;
+  dealInitialHands(): void;
+  computerPlayTurn(): {
+    card: ICard;
+    parameter: number | null;
+    result: boolean;
+  } | null;
+  updatePossibilities(forPlayer: boolean, card: ICard, parameter: number | null, result: boolean): void;
+}
